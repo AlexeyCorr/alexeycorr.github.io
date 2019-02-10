dfc2e052d7afa01b63c9c8d1a18f7d7876677106
AlexeyCorr <alexeycorr@gmail.com>
2019-02-11 02:00:24 +0300

    update site

--- portfolio/keksobooking/js/synchronize-fields.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-(function () {
-  window.synchronizeFields = function (fieldOne, fieldTwo, valuesOneField, valuesTwoField, syncValues) {
-    var selectedIndex = valuesOneField.indexOf(fieldOne.value);
-    syncValues(fieldTwo, valuesTwoField[selectedIndex]);
-  };
-})();
